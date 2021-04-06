48f498e657448fea972ef458b5edbfe09477f50e
Umar Khalilov <ERMASTER100@gmail.com>
2021-04-06 11:18:23 +0300

    Add refactor tasks and http query

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 const express = require('express');
-
-const router = require('./router');
+const router = require('./routers');
+const errorHandler = require('./middlewares/error.handlers');
 
 const app = express();
 
 app.use(express.json());
+
 app.use('/api', router);
 
-app.use((err, req, res, next) => {
-  console.log('Error caught: ->>>>>', err);
-  if (res.headerSent) {
-    return;
-  }
-  const status = err.status || 500;
-  const message = err.message || 'Server error';
-  res.status(status).send({ errors: [message] });
-});
+app.use(errorHandler);
 
 module.exports = app;
